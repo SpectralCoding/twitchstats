01d3a12526ee44f344b3be0ba1a6e23106d88848
Caesar Kabalan <caesar.kabalan@gmail.com>
2015-04-02 21:49:07 -0700

    Added StyleCop settings.

--- Statistician/Program.cs
+++ Statistician/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <copyright file="Program.cs" company="SpectralCoding.com">
+///     Copyright (c) 2015 SpectralCoding
+/// </copyright>
+/// <license>
+/// This file is part of TwitchStats.
+///
+/// IncreBuild is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// IncreBuild is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with TwitchStats.  If not, see <http://www.gnu.org/licenses/>.
+/// </license>
+/// <author>Caesar Kabalan</author>
 
 namespace Statistician {
-	class Program {
-		static void Main(string[] args) {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Program {
+		private static void Main(string[] args) {
 		}
 	}
 }
